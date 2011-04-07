bc5016096384af95b7f2da776e4cc07e07de1f61
Lance Speelmon <lance@speelmon.com>
2011-04-07 11:28:23 -0400

    KERN-1720 WIP removed test from derby

--- src/test/java/org/sakaiproject/nakamura/lite/jdbc/derby/ContentManagerManagerImplTest.java
+++ src/test/java/org/sakaiproject/nakamura/lite/jdbc/derby/ContentManagerManagerImplTest.java
@@ -17,24 +17,8 @@
  */
 package org.sakaiproject.nakamura.lite.jdbc.derby;
 
-import com.google.common.collect.ImmutableMap;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.sakaiproject.nakamura.api.lite.StorageClientException;
-import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
-import org.sakaiproject.nakamura.api.lite.authorizable.User;
-import org.sakaiproject.nakamura.api.lite.content.Content;
-import org.sakaiproject.nakamura.lite.LoggingStorageListener;
-import org.sakaiproject.nakamura.lite.accesscontrol.AccessControlManagerImpl;
-import org.sakaiproject.nakamura.lite.accesscontrol.AuthenticatorImpl;
 import org.sakaiproject.nakamura.lite.content.AbstractContentManagerTest;
-import org.sakaiproject.nakamura.lite.content.ContentManagerImpl;
 import org.sakaiproject.nakamura.lite.storage.StorageClientPool;
-
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
 
 public class ContentManagerManagerImplTest extends AbstractContentManagerTest {
 
@@ -43,61 +27,4 @@ public class ContentManagerManagerImplTest extends AbstractContentManagerTest {
     return DerbySetup.getClientPool();
   }
 
-  @Test
-  public void testMultiValuedIndexSearch() throws StorageClientException,
-      AccessDeniedException {
-    AuthenticatorImpl AuthenticatorImpl = new AuthenticatorImpl(client, configuration);
-    User currentUser = AuthenticatorImpl.authenticate("admin", "admin");
-
-    AccessControlManagerImpl accessControlManager = new AccessControlManagerImpl(client,
-        currentUser, configuration, null, new LoggingStorageListener(),
-        principalValidatorResolver);
-    ContentManagerImpl contentManager = new ContentManagerImpl(client,
-        accessControlManager, configuration, null, new LoggingStorageListener());
-    // add some content with multi-valued properties
-    final String propKey = "prop1";
-    final String[] multiValueA = new String[] { "valueA", "valueB" };
-    final String[] multiValueX = new String[] { "valueX", "valueY", "valueZ" };
-    contentManager.update(new Content("/testA", ImmutableMap.of(propKey,
-        (Object) multiValueA)));
-    contentManager.update(new Content("/testX", ImmutableMap.of(propKey,
-        (Object) multiValueX)));
-
-    // verify state of content
-    Content contentA = contentManager.get("/testA");
-    Content contentX = contentManager.get("/testX");
-    Assert.assertEquals("/testA", contentA.getPath());
-    Assert.assertEquals("/testX", contentX.getPath());
-    Map<String, Object> propsA = contentA.getProperties();
-    Map<String, Object> propsX = contentX.getProperties();
-    Assert.assertTrue(Arrays.equals(multiValueA, (String[]) propsA.get(propKey)));
-    Assert.assertTrue(Arrays.equals(multiValueX, (String[]) propsX.get(propKey)));
-
-    // now test index search; search for "a" find contentA
-    Map<String, Object> searchCriteria = ImmutableMap.of(propKey,
-        (Object) multiValueA);
-    Map<String, Object> orSet = ImmutableMap.of("orset0", (Object) searchCriteria);
-    Iterable<Content> iterable = contentManager.find(orSet);
-    Assert.assertNotNull("Iterable should not be null", iterable);
-    Iterator<Content> iter = iterable.iterator();
-    Assert.assertNotNull("Iterator should not be null", iter);
-    Assert.assertTrue("Should have found a match", iter.hasNext());
-    Content match = iter.next();
-    Assert.assertNotNull("match should not be null", match);
-    Assert.assertEquals("/testA", match.getPath());
-    Assert.assertNotNull("match should have key: " + propKey, match.getProperty(propKey));
-    Assert.assertTrue("String[] should be equal",
-        Arrays.equals(multiValueA, (String[]) match.getProperty(propKey)));
-    searchCriteria = ImmutableMap.of(propKey, (Object) multiValueA[1]);
-    iterable = contentManager.find(searchCriteria);
-    Assert.assertNotNull(iterable);
-    iter = iterable.iterator();
-    Assert.assertNotNull(iter);
-    Assert.assertTrue("Should have found a match", iter.hasNext());
-    match = iter.next();
-    Assert.assertNotNull(match);
-    Assert.assertNotNull(match.getProperty(propKey));
-    Assert.assertTrue(Arrays.equals(multiValueA,
-        (String[]) iter.next().getProperty(propKey)));
-  }
 }
